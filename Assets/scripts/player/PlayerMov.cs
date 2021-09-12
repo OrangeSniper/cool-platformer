333962a982c2a7c65cb2d3a6261caeee4fba28d9
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-09-12 06:59:41 -0400

    double jump mechanic works

--- Assets/scripts/player/PlayerMov.cs
+++ Assets/scripts/player/PlayerMov.cs
@@ -7,6 +7,9 @@ public class PlayerMov : MonoBehaviour
     public float jumpHeight;
     private Rigidbody2D rb;
     private PlayerInputActions inputActions;
+    public int curJumps;
+    public int jumps = 2;
+    public GameObject GCheckPoint;
 
     private void Awake()
     {
@@ -16,19 +19,41 @@ public class PlayerMov : MonoBehaviour
         inputActions.Movment.Enable();
 
     }
+    private void Start()
+    {
+        curJumps = jumps;
+    }
 
     private void FixedUpdate()
     {
         Vector2 inputVector = new Vector2(inputActions.Movment.Movment.ReadValue<float>() * speed, 0f);
         rb.velocity = new Vector2(inputVector.x * speed, rb.velocity.y);
+        Debug.Log(GCheck());
+        if(GCheck())
+        {
+            curJumps = jumps;
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
         Debug.Log("jump");
-        if(context.performed)
+        if(context.performed && curJumps > 1)
         {
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            curJumps--;
+        }
+    }
+
+    public bool GCheck()
+    {
+        RaycastHit2D cast = Physics2D.Raycast(GCheckPoint.transform.position, Vector2.down);
+        if(cast.distance <= .01f)
+        {
+            return true;
+        }else
+        {
+            return false;
         }
     }
 }
