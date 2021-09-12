d5a108c2796d9f5ddc8e3e93e62fe2809edf1f84
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-09-12 09:14:20 -0400

    tracks system works

--- Assets/scripts/game handling/Music.cs
+++ Assets/scripts/game handling/Music.cs
@@ -7,4 +7,35 @@ public class Music : MonoBehaviour
     public int curTrack;
     public GameObject[] enemies;
     public Track[] tracks;
+
+    private int curBPM;
+
+    private AudioSource source;
+
+    private float curSecBtwnBeat;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        curBPM = tracks[0].bpm;
+        StartCoroutine(MusicPlay());
+    }
+
+    IEnumerator MusicPlay()
+    {
+        curTrack = Random.Range(0, tracks.Length);
+        source.clip = tracks[curTrack].audio;
+        source.Play();
+        yield return new WaitForSeconds(getSecBtwnBeat(curBPM) * 4 * tracks[curTrack].lengthInBars);
+        StartCoroutine(MusicPlay());
+    }
+
+    public float getSecBtwnBeat(float bpm)
+    {
+        return 60000f / bpm / 1000f;
+    }
 }
