89b531219c0b3927890b09bb038bd411d3895e8f
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-09-20 10:10:21 -0400

    added scriptable asset

---
+++ Assets/scripts/script obj/Weapon.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "simple weapon", menuName = "my scriptable objects/simple weapon")]
+public class Weapon : ScriptableObject
+{
+    public string name;
+}
