ec70ddd5b45775397b6cf3b13d77533933551e1d
OrangeSniper <51276538+OrangeSniper@users.noreply.github.com>
2021-10-07 09:13:49 -0400

    still working on indv

---
+++ Assets/scripts/UI/DisplayInventory.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    public int xSpaceBtwnItems;
+    public int columns;
+    public int ySpaceBtwnItems;
+    Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDisplay();
+    }
+
+    private void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+
+    }
+
+    public Vector3 GetPosition(int i)
+    {
+        return new Vector3(xSpaceBtwnItems * (i%columns), -ySpaceBtwnItems * (i/columns), 0f);
+    }
+}
